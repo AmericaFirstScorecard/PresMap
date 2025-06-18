392e12f97973d5344b96bde7716173db804bffd5
egeezee <CalebJavanmardi0289@gmail.com>
2025-06-17 20:26:10 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoiNW00Y2s3NyIsImEiOiJjbWI4eXFqeDkwbzY1MmpwcDFzZDIwMmVqIn0.6JGe7JWhk28z5D3TLIJQwg';
+export const STATE_CSV_URL = 'https://docs.google.com/spreadsheets/d/10zbHap425MtnS4jgsELt__DhtEQbXI8GlnD-6vkB4yg/export?format=csv';
+export const COUNTY_CSV_URL = 'https://docs.google.com/spreadsheets/d/1edopYcOoeeom30K3WsCmzVwvJ8rHcdYH7xB7B7QCQBI/export?format=csv';
